4e82cc66b2bd13034efcadd6db64d777d405b826
Lindsay <141155135+LindsayJamesMartin-TomTom@users.noreply.github.com>
2024-12-05 09:46:03 +0100

    Day 1 solution

--- Day1.kt
+++ Day1.kt
@@ -1,3 +1,46 @@
+import java.io.File
+import kotlin.math.abs
+
+fun readInput() = File("input.txt").useLines{ it.toList() }
+
+fun firstPart(rawInput: List<String>){
+  val left = mutableListOf<Int>()
+  val right = mutableListOf<Int>()
+  for (line in rawInput) {
+    val pair = line.split("   ")
+    left.add(pair[0].toInt())
+    right.add(pair[1].toInt())
+  }
+  left.toIntArray()
+  right.toIntArray()
+  left.sort()
+  right.sort()
+  var answer = 0
+  left.forEachIndexed{i, item ->
+    answer += abs(item - right[i])
+  }
+  println("Answer 1: $answer")
+}
+
+fun secondPart(rawInput: List<String>){
+  val left = mutableListOf<Int>()
+  val right = mutableListOf<Int>()
+  for (line in rawInput) {
+    val pair = line.split("   ")
+    left.add(pair[0].toInt())
+    right.add(pair[1].toInt())
+  }
+  left.toIntArray()
+  right.toIntArray()
+  var answer = 0
+  left.forEachIndexed{i, item ->
+    answer += (item * right.count{element -> element == item})
+  }
+  println("Answer 2: $answer")
+}
+
 fun main (args: Array<String>) {
-  println("Test")
+  val rawInput = readInput()
+  firstPart(rawInput)
+  secondPart(rawInput)
 }
